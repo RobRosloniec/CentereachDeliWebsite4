6318c1d7efcb5d821c2a9890195ec03e24d89124
RobRosloniec <robrosloniec@gmail.com>
2024-02-06 15:26:38 -0500

    Initial Setup

---
+++ centereach-deli-website/src/components/Homescreen/Homescreen.js
@@ -0,0 +1,12 @@
+import breakfast from '../../data/breakfast.json'
+import lunch     from '../../data/lunch.json'
+const Homescreen = () => {
+    console.log(breakfast,lunch)
+    return(
+        <div className='bg-slate-500'>
+            Shalome
+        </div>
+    )
+}
+
+export default Homescreen
